bd7f4017dfbec5fa71484351499ff317517101bd
Андрей Рашев <ar42698937@edu.mon.bg>
2024-06-17 17:06:52 +0300

    Scaffolded Controlls and views

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace FireSafetyManager
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
             // Add services to the container.
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
@@ -18,7 +19,9 @@ namespace FireSafetyManager
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
             builder.Services.AddControllersWithViews();
+            builder.Services.AddRazorPages();
 
             var app = builder.Build();
 
@@ -30,7 +33,6 @@ namespace FireSafetyManager
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -39,13 +41,13 @@ namespace FireSafetyManager
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
             app.MapRazorPages();
-
             app.Run();
         }
     }
